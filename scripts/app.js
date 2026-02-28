1a370d08c30ddb97f27018d942c55ab41e9ecd48
rdelaz01 <rolliejohn.delacruz@gmail.com>
2026-02-28 09:48:04 -0800

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+//function hello() {
+    //console.log("Hello, World!");
+//}
+
+
+//// An example of changing logic exec
+//function init () {
+//    hello();
+//    console.log("Helllo this is the DOM");
+//}
+//force my logic to run the html and css first  - and when they finsh
+// will be executed 
+
+//window.onload = init; 
+
+PSEUDOCODE: Initialization
+===========================
+
+FUNCTION init():
+    PRINT "App initialized"
+  // We will attach events here in the next session
+END FUNCTION
+
+WHEN page finishes loading:
+    CALL init()
